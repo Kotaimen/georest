9a9ae2e03e8ef678f70ef6e059065f2292aacd1d
Kotaimen <kotaimen.c@gmail.com>
2014-03-23 21:49:16 +0800

    Don't export exceptions in __init__

--- georest/geo/__init__.py
+++ georest/geo/__init__.py
@@ -11,6 +11,5 @@
 __author__ = 'kotaimen'
 __date__ = '3/18/14'
 
-from .exception import GeoException
 from .geometry import Geometry, build_geometry, build_srs
 from .feature import Feature, build_feature
