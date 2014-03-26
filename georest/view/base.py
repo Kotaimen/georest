e7951e30aab4e225d6000b11b8725e16925dece0
Kotaimen <kotaimen.c@gmail.com>
2014-03-26 15:43:42 +0800

    - Move geometry argument parser to georest.view.base - Move geo exception handler to decorator to simplify api code, and consistent error handling

--- georest/view/base.py
+++ georest/view/base.py
@@ -11,74 +11,110 @@ __date__ = '3/21/14'
 import datetime
 
 from flask import current_app, make_response
-from flask.ext.restful import Resource, abort
+from flask.ext.restful import Resource, abort, reqparse
 
+from ..geo.exception import GeoException, InvalidGeometry, \
+    InvalidCoordinateReferenceSystem
 from ..store.exception import GeometryAlreadyExists, InvalidKey, \
     GeometryDoesNotExist
-from ..geo.exception import InvalidGeometry, InvalidCoordinateReferenceSystem
+
 from ..geo import Geometry
+
+#
+# Response Helpers
+#
+
+def make_header_from_feature(feature):
+    """ Generate a header using feature metadata"""
+    age = current_app.config['EXPIRES']
+    expires = feature.modified + datetime.timedelta(seconds=age)
+    date_format = current_app.config['DATE_FORMAT']
+    return {
+        'Etag': feature.etag,
+        'Date': feature.created.strftime(date_format),
+        'Last-Modified': feature.modified.strftime(date_format),
+        'Cache-Control': 'max-age=%d,must-revalidate' % age,
+        'Expires': expires.strftime(date_format), }
+
+
+def make_response_from_geometry(geometry, format_='json', srid=0, headers={}):
+    """ Make a geometry response """
+    assert isinstance(geometry, Geometry)
+
+    if srid:
+        try:
+            geometry.transform(srid)
+        except GeoException as e:
+            raise InvalidCoordinateReferenceSystem(e)
+
+    if format_ == 'ewkt':
+        response_data = geometry.ewkt
+        content_type = 'text/plain'
+    elif format_ == 'ewkb':
+        response_data = str(geometry.ewkb)
+        content_type = 'application/oct-stream'
+    if format_ == 'json':
+        response_data = geometry.json
+        content_type = 'application/json'
+
+    response = make_response(response_data, 200)
+    response.headers['Content-Type'] = content_type
+
+    for k, v in headers.iteritems():
+        response.headers[k] = v
+
+    return response
+
+
+def throws_geo_exceptions(f):
+    def decorator(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception as e:
+            if isinstance(e,
+                          (InvalidGeometry, InvalidCoordinateReferenceSystem)):
+                abort(400, message='Invalid geometry: %s' % e.message)
+            elif isinstance(e, GeometryDoesNotExist):
+                abort(404, message='Key does not exist: "%s"' % e.message)
+            elif isinstance(e, InvalidKey):
+                abort(400, message='Invalid key: "%s"' % e.message)
+            elif isinstance(e, GeometryAlreadyExists):
+                abort(409, message='Geometry exists: "%s"' % e.message)
+            elif isinstance(e, GeoException):
+                abort(500, message='Geometry exception: "%s"' % e.message)
+            else:
+                raise
+
+    return decorator
 
 
 class BaseResource(Resource):
     """ Geo resource base """
+
+    method_decorators = [throws_geo_exceptions]
 
     def __init__(self):
         super(BaseResource, self).__init__()
         # Inject model
         self.model = current_app.model
 
-    def abort(self, e):
-        """ Generate status code and error message using Exception types """
-        current_app.logger.exception(e)
-        # exc_type, exc_value, exc_traceback = e.exc_info
-        # tb = traceback.format_exc(exc_traceback)
-        if isinstance(e, (InvalidGeometry, InvalidCoordinateReferenceSystem)):
-            abort(400, message='Invalid geometry: %s' % e.message)
-        elif isinstance(e, GeometryDoesNotExist):
-            abort(404, message='Key does not exist: "%s"' % e.message)
-        elif isinstance(e, InvalidKey):
-            abort(400, message='Invalid key: "%s"' % e.message)
-        elif isinstance(e, GeometryAlreadyExists):
-            abort(409, message='Geometry exists: "%s"' % e.message)
-        else:
-            raise
 
-    def make_header_from_feature(self, feature):
-        """ Generate a header using feature metadata"""
-        age = current_app.config['EXPIRES']
-        expires = feature.modified + datetime.timedelta(seconds=age)
-        date_format = current_app.config['DATE_FORMAT']
-        return {
-            'Etag': feature.etag,
-            'Date': feature.created.strftime(date_format),
-            'Last-Modified': feature.modified.strftime(date_format),
-            'Cache-Control': 'max-age=%d,must-revalidate' % age,
-            'Expires': expires.strftime(date_format),
-        }
+class GeometryRequestParser(reqparse.RequestParser):
+    def __init__(self):
+        super(GeometryRequestParser, self).__init__()
 
-    def make_response_from_geometry(self, geometry, format_, srid, headers):
-        """ Make a geometry response """
-        assert isinstance(geometry, Geometry)
-
-        if srid:
-            geometry.transform(srid)
-
-        if format_ == 'ewkt':
-            response_data = geometry.ewkt
-            content_type = 'text/plain'
-        elif format_ == 'ewkb':
-            response_data = str(geometry.ewkb)
-            content_type = 'application/oct-stream'
-        if format_ == 'json':
-            response_data = geometry.json
-            content_type = 'application/json'
-
-        response = make_response(response_data, 200)
-        response.headers['Content-Type'] = content_type
-
-        for k, v in headers.iteritems():
-            response.headers[k] = v
-
-        return response
-
-
+        self.add_argument('format',
+                          dest='format',
+                          action='store',
+                          location='args',
+                          type=str,
+                          choices=['json', 'ewkt', 'ewkb'],
+                          default='json',
+                          required=False)
+        self.add_argument('srid',
+                          dest='srid',
+                          action='store',
+                          location='args',
+                          type=str,
+                          default=0,
+                          required=False)
--- georest/view/geores.py
+++ georest/view/geores.py
@@ -7,9 +7,9 @@ import json
 
 from flask import make_response, request
 from flask.ext.restful import marshal_with, abort
-from flask.ext.restful.reqparse import RequestParser
 
-from .base import BaseResource
+from .base import BaseResource, make_header_from_feature, \
+    make_response_from_geometry, GeometryRequestParser
 from .fields import FEATURE_FIELDS
 
 from ..geo.exception import GeoException
@@ -24,62 +24,35 @@ class Stat(BaseResource):
 
 
 class GeometryResource(BaseResource):
-    parser = RequestParser()
-    parser.add_argument('format',
-                        dest='format',
-                        action='store',
-                        location='args',
-                        type=str,
-                        choices=['json', 'ewkt', 'ewkb'],
-                        default='json',
-                        required=False)
-    parser.add_argument('srid',
-                        dest='srid',
-                        action='store',
-                        location='args',
-                        type=str,
-                        default=0,
-                        required=False)
+    parser = GeometryRequestParser()
+
 
     def get(self, key):
         args = self.parser.parse_args()
-        try:
-            feature = self.model.get_feature(key)
-        except GeoException as e:
-            self.abort(e)
-
-        headers = self.make_header_from_feature(feature)
-
-        return self.make_response_from_geometry(feature.geometry,
-                                                args.format,
-                                                args.srid,
-                                                headers)
+        feature = self.model.get_feature(key)
+        headers = make_header_from_feature(feature)
+        return make_response_from_geometry(feature.geometry,
+                                           args.format,
+                                           args.srid,
+                                           headers)
 
     def post(self, key):
         data = request.data
-        try:
-            feature = self.model.put_feature(key, data)
-        except GeoException as e:
-            self.abort(e)
-
-        return None, 201, self.make_header_from_feature(feature)
+        feature = self.model.put_feature(key, data)
+        return None, 201, make_header_from_feature(feature)
 
     def delete(self, key):
-        try:
-            self.model.delete_feature(key)
-        except GeoException as e:
-            self.abort(e)
+        self.model.delete_feature(key)
         return None, 200
 
 
 class FeatureResource(BaseResource):
     @marshal_with(FEATURE_FIELDS)
     def get(self, key):
-        try:
-            feature = self.model.get_feature(key)
-        except GeoException as e:
-            self.abort(e)
-        return feature, 200, self.make_header_from_feature(feature)
+        feature = self.model.get_feature(key)
+        return feature, 200, make_header_from_feature(feature)
+
+
 
 
 
