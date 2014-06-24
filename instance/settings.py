9632ce3cb110a3a512de4dfc864e2d98e9935c7d
heping <heping@pset.suntec.net>
2014-06-24 17:18:43 +0800

    add test storage config to instance settings

--- instance/settings.py
+++ instance/settings.py
@@ -3,3 +3,12 @@
 """
     App instance specific settings
 """
+
+
+STORAGE = {
+    'prototype': 'postgis',
+    'host': '172.26.183.193',
+    'username': 'postgres',
+    'password': '123456',
+    'database': 'georest-test',
+}
